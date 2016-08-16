ad253bcaefc85107b5876afd64677fd012173caf
rnazaryan <stenopage@me.com>
2016-08-15 18:19:02 -0700

    Initial Commit

---
+++ tester/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tester
+//
+//  Created by Raffi Nazaryan on 8/15/16.
+//  Copyright © 2016 JRLR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
